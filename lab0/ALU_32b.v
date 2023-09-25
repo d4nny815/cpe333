3fa221fd7c874ea6bd1b279d5e3e15c7872fd808
danny <d4nny815@gmail.com>
2023-09-24 19:00:25 -0700

    comments

--- lab0/ALU_32b.v
+++ lab0/ALU_32b.v
@@ -5,13 +5,13 @@ module ALU_32b (
     output reg [31:0] Result,
     output Zero
 );
+
     always @(*) begin
         case (ALUOp)
             3'b000: Result = A & B;
             3'b001: Result = A | B;
             3'b010: Result = A + B;
             3'b110: Result = A - B;
-//            3'b111: Result = A < B;
             3'b111: Result = $signed(A) < $signed(B);
             default: Result = 0;
         endcase
