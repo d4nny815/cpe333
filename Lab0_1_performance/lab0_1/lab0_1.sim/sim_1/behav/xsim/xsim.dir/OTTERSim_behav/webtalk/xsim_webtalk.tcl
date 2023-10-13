2a5e3df8cca3bbf73de16185df5a564d4c9f5c31
danny <d4nny815@gmail.com>
2023-10-12 21:17:31 -0700

    l

--- Lab0_1_performance/lab0_1/lab0_1.sim/sim_1/behav/xsim/xsim.dir/OTTERSim_behav/webtalk/xsim_webtalk.tcl
+++ Lab0_1_performance/lab0_1/lab0_1.sim/sim_1/behav/xsim/xsim.dir/OTTERSim_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
 webtalk_init -webtalk_dir Z:/schoolfiles/school_files/cpe333/Lab0_1_performance/lab0_1/lab0_1.sim/sim_1/behav/xsim/xsim.dir/OTTERSim_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Mon Oct  2 22:30:22 2023" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Wed Oct  4 21:52:30 2023" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2020.2 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "3064766" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
@@ -14,7 +14,7 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "8ce32c12-9b23-4a66-9ff8-1679d5de177d" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "ef0cc77942ca4ab884980f8f1c343e5e" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "53" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "64" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Windows Server 2016 or Windows 10" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "major release  (build 9200)" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "AMD Ryzen 9 7950X3D 16-Core Processor          " -context "user_environment"
@@ -24,9 +24,9 @@ webtalk_add_data -client project -key system_ram -value "33.000 GB" -context "us
 webtalk_register_client -client xsim
 webtalk_add_data -client xsim -key Command -value "xsim" -context "xsim\\command_line_options"
 webtalk_add_data -client xsim -key trace_waveform -value "true" -context "xsim\\usage"
-webtalk_add_data -client xsim -key runtime -value "501 us" -context "xsim\\usage"
+webtalk_add_data -client xsim -key runtime -value "1 us" -context "xsim\\usage"
 webtalk_add_data -client xsim -key iteration -value "1" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Time -value "0.05_sec" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Memory -value "10776_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 3463937962 -regid "" -xml Z:/schoolfiles/school_files/cpe333/Lab0_1_performance/lab0_1/lab0_1.sim/sim_1/behav/xsim/xsim.dir/OTTERSim_behav/webtalk/usage_statistics_ext_xsim.xml -html Z:/schoolfiles/school_files/cpe333/Lab0_1_performance/lab0_1/lab0_1.sim/sim_1/behav/xsim/xsim.dir/OTTERSim_behav/webtalk/usage_statistics_ext_xsim.html -wdm Z:/schoolfiles/school_files/cpe333/Lab0_1_performance/lab0_1/lab0_1.sim/sim_1/behav/xsim/xsim.dir/OTTERSim_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_add_data -client xsim -key Simulation_Time -value "0.00_sec" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Simulation_Memory -value "7680_KB" -context "xsim\\usage"
+webtalk_transmit -clientid 3216974900 -regid "" -xml Z:/schoolfiles/school_files/cpe333/Lab0_1_performance/lab0_1/lab0_1.sim/sim_1/behav/xsim/xsim.dir/OTTERSim_behav/webtalk/usage_statistics_ext_xsim.xml -html Z:/schoolfiles/school_files/cpe333/Lab0_1_performance/lab0_1/lab0_1.sim/sim_1/behav/xsim/xsim.dir/OTTERSim_behav/webtalk/usage_statistics_ext_xsim.html -wdm Z:/schoolfiles/school_files/cpe333/Lab0_1_performance/lab0_1/lab0_1.sim/sim_1/behav/xsim/xsim.dir/OTTERSim_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
